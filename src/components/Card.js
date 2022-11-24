f7902fa7865ee7c2fcb9cf0201f25ff3a5eb2a4e
KobiManuel <Emmakobi91@gmail.com>
2022-11-24 18:49:54 +0100

    feat: passed up data from NewExpense back up to App.js with data inheritance

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
-/*import '../stylesheets/Card.css'
-function Card(props) {
-    const classes = 'card' + props.className;
-  return (
-    <div className={classes}>{props.children}</div>
-  )
-}
-export default Card */
+import '../stylesheets/Card.css';
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
